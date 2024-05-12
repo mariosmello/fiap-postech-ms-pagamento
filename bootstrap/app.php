8ab8606ab4adf91c1c334519ba459b67344a7b1d
Mário Mello <marioesmello@gmail.coim>
2024-05-12 16:14:53 -0300

    Cria recurso para Webhook e disparo de eventos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\EnsureTokenIsValid::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (\Illuminate\Validation\UnauthorizedException $e) {
